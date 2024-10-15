49cfbef4a57274b3674aa4c8c9ff23294fdbdd27
Chad Schultz <25645350+itoleck@users.noreply.github.com>
2024-10-15 10:08:20 -0500

    Update movemovies script

--- MoveMovies.ps1
+++ MoveMovies.ps1
@@ -1,12 +1,16 @@
 #Move files work in progress scripts
 
-$FolderPath = "P:\movies"
+param (
+    [Parameter(Mandatory=$true)][System.IO.DirectoryInfo] $Path
+)
+
+$FolderPath = $Path
 $files = Get-ChildItem -Path $FolderPath -File #| Select-Object -First 1   #For testing
 foreach ($file in $files) {
     Move-Item -Path $file.FullName -Destination "$FolderPath\$($file.BaseName)\"
 }
 
-$FolderPath = "P:\movies"
+$FolderPath = $Path
 $files = Get-ChildItem -Path $FolderPath -File #| Select-Object -First 10   #For testing
 foreach ($file in $files) {
     if ($file.Extension.Length -lt 1) {
